3db45af2c7accef2239599cf46aa6a54cc80f8d4
Danster4 <86613317+Danster4@users.noreply.github.com>
2022-01-11 10:16:19 -0500

    updated resume with information and css styling for mobile and desktop

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -39,7 +39,7 @@ function Resume() {
             hover:border-green-400 hover:bg-green-800 hover:text-white
             transition" 
             type="submit">
-              Download Resume
+              Download
             </a>
         
             <button
@@ -56,70 +56,126 @@ function Resume() {
             hover:border-orange-400 hover:bg-orange-800 hover:text-white
             transition" 
             type="submit">
-              Preview Resume
+              Preview
             </button>
         </div>
 
+    <div class="flex flex-wrap -mx-px overflow-hidden xl:-mx-10">
 
-
-    <div className="flex flex-wrap overflow-hidden justify-center grid-cols-2">
-
-      <div className="w-full  overflow-hidden flex justify-center items-start lg:pt-10 xl:pt-10">
-        <h2 className='text-white text-3xl mb-6 gap-3'>
+      <div class="my-px px-px w-full overflow-hidden md:w-1/2 lg:w-1/2 xl:w-1/2 xl:my-10 xl:px-10">
+        <h2 className='text-white text-3xl'>
           <em>Work Experience</em>
         </h2>
-        <p className="grid flex gap-3 text-white text-xl mx-5 lg:mx-20 xl:mx-[90px]">
-          Web Development Intern          
-          <br className='sm:hidden md:hidden lg:hidden xl:hidden'/> <br className='sm:hidden md:hidden lg:hidden xl:hidden'/> 
-          Guest Services Ambassador
+        <br />
+        <p className="text-white text-xl">
+          <strong>Web Development Intern</strong>          
+          <br /> 
+          Waterman Bank
+          <br />
+          Oct. 2021 - Present
         </p>
         <br />
-        <h2 className='text-white text-3xl mb-6 gap-3'>
+        <p className="text-white text-xl">
+          <strong>Guest Services Ambassador</strong>          
+          <br /> 
+          Hotel Haya/Aparium
+          <br />
+          Jun. 2021 - Nov. 2021
+        </p>
+        <br /> 
+        <h2 className='text-white text-3xl'>
           <em>Education</em>
         </h2>
-        <p className="grid flex gap-3 text-white text-xl mx-5 lg:mx-20 xl:mx-[90px]">
-          Phone: (312) 391-7134 
+        <br />
+        <p className="text-white text-xl">
+          <strong>Certificate</strong>          
+          <br /> 
+          University of Central Florida <br/>Coding Boot Camp
           <br />
-          Email: danielurkov@gmail.com
-        </p>
-      </div>
-
-      <div className="w-full overflow-hidden flex justify-center items-start lg:pt-10 xl:pt-10">
-        <h2 className='text-white text-3xl mb-6 gap-3'>
-          <em>Languages</em>
-        </h2>
-        <p className="grid flex gap-3 text-white text-xl mx-5 lg:mx-20 xl:mx-[90px]">
-          Phone: (312) 391-7134 
-          <br />
-          Email: danielurkov@gmail.com
+          Aug. 2021 - Present
         </p>
         <br />
-        <h2 className='text-white text-3xl mb-6 gap-3'>
-          <em>UI/UX and Technical Skills</em>
-        </h2>
-        <p className="grid flex gap-3 text-white text-xl mx-5 lg:mx-20 xl:mx-[90px]">
-          Phone: (312) 391-7134 
+        <p className="text-white text-xl">
+          <strong>Master of Arts<br />in Clinical Psychology</strong>          
+          <br /> 
+          National Louis University
           <br />
-          Email: danielurkov@gmail.com
+          Aug. 2018 - Dec. 2020
         </p>
-
-        
-        
-        
-        
+        <br />
+        <p className="text-white text-xl">
+          <strong>Bachelor of Science<br />in Psychology</strong>          
+          <br /> 
+          University of Miami
+          <br />
+          Aug. 2015 - Jun. 2018
+        </p>
+        <br />
       </div>
-     
-      {/* Resume svg, change into preview modal */}
-        {/* <div class="w-full overflow-hidden  mx-auto">
-          <div class="p-6">
-            <img src={resume} alt="resume image" />
-          </div>
-        </div> */}
 
+      
 
+      <div class="my-px px-px w-full overflow-hidden md:w-1/2 lg:w-1/2 xl:w-1/2 xl:my-10 xl:px-10">
+        <h2 className='text-white text-3xl'>
+          <em>Languages</em>
+        </h2>
+        <br />
+        <p className="text-white text-xl">
+          HTML5          
+          <br />
+          CSS3
+          <br />
+          JavaScript
+          <br />
+          jQuery
+          <br />
+          JSX
+          <br />
+          SQL
+        </p>
+        <br /> 
+        <h2 className='text-white text-3xl'>
+          <em>UI/UX & Dev Tools</em>
+        </h2>
+        <p className="text-white text-xl">
+          <br />
+          React.js          
+          <br />
+          Node.js
+          <br />
+          Express.js
+          <br />
+          Handlebars.js
+          <br />
+          WordPress
+          <br />
+          GitHub
+          <br />
+          Insomnia
+          <br />
+          Jest
+        </p>
+        <br />
+        <h2 className='text-white text-3xl'>
+          <em>Courses/Trainings</em>
+        </h2>
+        <br />
+        <p className="text-white text-xl">
+          <strong>Figma UI/UX Design Essentials</strong>          
+          <br /> 
+          udemy.com - Daniel Scott
+          <br />
+          Dec. 2021 - Present
+        </p>
+      </div>
 
     </div>
+
     
+     
+
+
+
   
 
       
